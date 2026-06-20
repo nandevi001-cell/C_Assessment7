507d65b73031fbd2860f58814bcebbd292f76a74
nandevi001-cell <nandevi001@gmail.com>
2026-06-20 20:50:08 +0530

    Create C_program_24.c

---
+++ C_program_24.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void disp_single_digit_square(int);
+int main()
+{
+	int x;
+	disp_single_digit_square(x);
+}
+void disp_single_digit_square(int x)
+{
+	int count=0,i;
+	printf("Enter a number:");
+	scanf("%d",&x);
+	while(x>0)
+	{
+		i=x%100;
+		if(i==16 || i==25 || i==36 || i==49 || i==64 || i==81)
+		{
+			count=count+1;
+		}
+		x=x/10;
+	}
+	printf("Enter the total number of two digit perfect square number:%d",count);
+}
