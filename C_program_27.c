29a56ff45618eb24c525df046fdfd6f7082e847e
nandevi001-cell <nandevi001@gmail.com>
2026-06-20 21:10:10 +0530

    Create C_program_27.c

---
+++ C_program_27.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void disp_count_sum(int);
+int main()
+{
+	int x;
+	disp_disp_count_sum(x);
+}
+void disp_disp_count_sum(int x)
+{
+    int n, digit, sum, count = 0;
+
+    x = 1;
+
+    while(x < 10000)
+    {
+        n = x;
+        sum = 0;
+        while(n > 0)
+        {
+            digit = n % 10;
+            sum = sum + digit;
+            n = n / 10;
+        }
+
+        if(sum == 14)
+        {
+            count = count + 1;
+        }
+
+        x = x + 1;
+    }
+
+    printf("Count = %d", count);
+}
