b2f888747af15cb60cf35237043e96ab8da329e3
nandevi001-cell <nandevi001@gmail.com>
2026-06-20 20:51:29 +0530

    Rename function from disp_single_digit_square to disp_two_digit_square

--- C_program_24.c
+++ C_program_24.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-void disp_single_digit_square(int);
+void disp_two_digit_square(int);
 int main()
 {
 	int x;
 	disp_single_digit_square(x);
 }
-void disp_single_digit_square(int x)
+void disp_two_digit_square(int x)
 {
 	int count=0,i;
 	printf("Enter a number:");
